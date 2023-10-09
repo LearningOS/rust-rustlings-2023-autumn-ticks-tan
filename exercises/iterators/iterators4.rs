66861dbff8018cb58def132aff30f12b347e0777
Ticks <ticks.cc@gmail.com>
2023-10-10 00:06:46 +0800

    [fix] finish 65-75 exercises

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +14,12 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if (num <= 1) {
+        1
+    }else {
+        let mut count = num;
+        std::iter::successors(Some(num), |n| if count > 2 {count -= 1; Some((n * count) as u64)}else{None} ).collect::<Vec<_>>().pop().unwrap()
+    }
 }
 
 #[cfg(test)]
